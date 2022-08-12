bd1bafbb7651fef7e04a68fb7f2dc06c10c320b0
Denis Fedorov <ssboshen@yandex.ru>
2022-08-12 20:45:47 +0300

    Test is Ok

--- src/test/java/ru/netology/patient/service/medical/MedicalServiceTest.java
+++ src/test/java/ru/netology/patient/service/medical/MedicalServiceTest.java
@@ -1,22 +1,24 @@
 package ru.netology.patient.service.medical;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import ru.netology.patient.entity.BloodPressure;
 import ru.netology.patient.entity.HealthInfo;
 import ru.netology.patient.entity.PatientInfo;
 import ru.netology.patient.repository.PatientInfoRepository;
 import ru.netology.patient.service.alert.SendAlertService;
-import ru.netology.patient.service.alert.SendAlertServiceImpl;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class MedicalServiceTest {
@@ -24,8 +26,9 @@ public class MedicalServiceTest {
     public static PatientInfoRepository patientInfoRepository;
     public static SendAlertService sendAlert;
 
-    @BeforeAll
-    public static void start(){
+
+    @BeforeEach
+    public void init() {
         patientInfoRepository = Mockito.mock(PatientInfoRepository.class);
         Mockito.when(patientInfoRepository.getById("4ab2fe14-bf25-4e53-ac16-5003c7c407b1"))
                 .thenReturn(
@@ -41,11 +44,7 @@ public class MedicalServiceTest {
                 );
 
         sendAlert = Mockito.mock(SendAlertService.class);
-        Mockito.when(sendAlert.send("Warning, patient with id: 4ab2fe14-bf25-4e53-ac16-5003c7c407b1, need help")).thenReturn(null);
-    }
 
-    @BeforeEach
-    public void init(){
         medService = new MedicalServiceImpl(patientInfoRepository, sendAlert);
     }
 
@@ -72,13 +71,23 @@ public class MedicalServiceTest {
 
     public static Stream<Arguments> sourceTemperature() {
         return Stream.of(
-                Arguments.of("4ab2fe14-bf25-4e53-ac16-5003c7c407b1", new BigDecimal("37.65"), 0),
-                Arguments.of("4ab2fe14-bf25-4e53-ac16-5003c7c407b1", new BigDecimal("40.65"), 1)
+                Arguments.of("4ab2fe14-bf25-4e53-ac16-5003c7c407b1", new BigDecimal("36.65"), 0),
+                Arguments.of("4ab2fe14-bf25-4e53-ac16-5003c7c407b1", new BigDecimal("4.65"), 1)
         );
+    }
+
+    @Test
+    public void testSendAlertService() {
+        ArgumentCaptor<String> aCaptor = ArgumentCaptor.forClass(String.class);
+        medService.checkBloodPressure("4ab2fe14-bf25-4e53-ac16-5003c7c407b1", new BloodPressure(120, 100));
+        Mockito.verify(sendAlert).send(aCaptor.capture());
+        assertEquals("Warning, patient with id: null, need help", aCaptor.getValue());
     }
 
     @AfterEach
     public void finalized() {
         medService = null;
+        sendAlert = null;
+        patientInfoRepository = null;
     }
 }
